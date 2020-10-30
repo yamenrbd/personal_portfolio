a4ad98321e516596c37af41ba7b8ab628f9f21c1
yamen <yamenrbd@hotmail.com>
2020-10-30 19:06:37 +0300

    second

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
+
+def all_blogs (request):
+    blogs = Blog.objects.order_by('-date')[:5]
+    return render (request , 'blog/all_blogs.html', {'blogs':blogs})
